5a5e5a9b850bb740a8d73a0648556295ddefc0a7
Eugene Zolotko <eugene.zolotko@globallogic.com>
2018-03-20 11:20:25 +0200

    Updated the test app per suggestion in https://issuetracker.google.com/issues/75245754#comment3.

--- app/src/main/java/com/example/eugenezolotko/blegattservertestapp/MainActivity.java
+++ app/src/main/java/com/example/eugenezolotko/blegattservertestapp/MainActivity.java
@@ -6,12 +6,17 @@ import android.bluetooth.BluetoothGattServer;
 import android.bluetooth.BluetoothGattServerCallback;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final UUID INCLUDED_SERVICE_1_UUID =
+            UUID.fromString("00001236-0000-1000-8000-00805f9b34fb");
+    private static final UUID SERVICE_1_UUID =
+            UUID.fromString("00001234-0000-1000-8000-00805f9b34fb");
 
     private BluetoothGattServer server;
 
@@ -36,26 +41,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createServices() {
-        final BluetoothGattService service1 = new BluetoothGattService(
-                UUID.fromString("00001234-0000-1000-8000-00805f9b34fb"),
-                BluetoothGattService.SERVICE_TYPE_PRIMARY);
-        final BluetoothGattCharacteristic characteristic1 = new BluetoothGattCharacteristic(
-                UUID.fromString("00001235-0000-1000-8000-00805f9b34fb"),
-                BluetoothGattCharacteristic.PROPERTY_READ,
-                BluetoothGattCharacteristic.PERMISSION_READ);
-        service1.addCharacteristic(characteristic1);
-
         final BluetoothGattService includedService1 = new BluetoothGattService(
-                UUID.fromString("00001236-0000-1000-8000-00805f9b34fb"),
+                INCLUDED_SERVICE_1_UUID,
                 BluetoothGattService.SERVICE_TYPE_SECONDARY);
         final BluetoothGattCharacteristic characteristic2 = new BluetoothGattCharacteristic(
                 UUID.fromString("00001237-0000-1000-8000-00805f9b34fb"),
                 BluetoothGattCharacteristic.PROPERTY_READ,
                 BluetoothGattCharacteristic.PERMISSION_READ);
         includedService1.addCharacteristic(characteristic2);
-        service1.addService(includedService1);
         server.addService(includedService1);
-        server.addService(service1);
     }
 
     private void closeServer() {
@@ -71,7 +65,18 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onServiceAdded(int status, BluetoothGattService service) {
-            super.onServiceAdded(status, service);
+            if (service.getUuid().equals(INCLUDED_SERVICE_1_UUID)) {
+                final BluetoothGattService service1 = new BluetoothGattService(
+                        SERVICE_1_UUID,
+                        BluetoothGattService.SERVICE_TYPE_PRIMARY);
+                final BluetoothGattCharacteristic characteristic1 = new BluetoothGattCharacteristic(
+                        UUID.fromString("00001235-0000-1000-8000-00805f9b34fb"),
+                        BluetoothGattCharacteristic.PROPERTY_READ,
+                        BluetoothGattCharacteristic.PERMISSION_READ);
+                service1.addService(service);
+                service1.addCharacteristic(characteristic1);
+                server.addService(service1);
+            }
         }
     };
 }
